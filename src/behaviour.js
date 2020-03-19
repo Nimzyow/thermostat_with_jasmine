64b071579b14c97f216ab61b5f412c2c2d4f9cf5
Nimzyow <n_soufiani@hotmail.com>
2020-03-19 22:05:11 +0000

    added feature to change locations

--- src/behaviour.js
+++ src/behaviour.js
@@ -9,7 +9,20 @@ $(document).ready(function() {
     }).responseText
   );
 
+  $("#current-temperature").text(data.main.temp + " degrees C");
+
   console.log(data.main.temp);
+
+  $("#current-city").change(function() {
+    let city = $("#current-city").val();
+    $.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`,
+      function(data) {
+        $("#current-temperature").text(data.main.temp);
+        $("#city").text(city);
+      }
+    );
+  });
 
   $("#incrementButton").on("click", function() {
     thermostat.increase();
